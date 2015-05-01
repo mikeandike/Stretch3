027fd8436a7d42b1bb828a42e493ece4e28e934d
Michael Sacks <michaelsacks@gmail.com>
2015-05-01 09:22:59 -0600

    Initial Commit

---
+++ Stretch3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Stretch3
+//
+//  Created by Michael Sacks on 5/1/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Stretch3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stretch3
+//
+//  Created by Michael Sacks on 5/1/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
